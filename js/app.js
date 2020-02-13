173b4bb0d0b1d1ebb780fc10e1ce5c620771368d
francescodt <francescodtroiano@gmail.com>
2020-02-12 22:12:48 -0600

    lab work

--- js/app.js
+++ js/app.js
@@ -1,21 +1,34 @@
 console.log('This is connected and working');
 
 
-
-var hourNow = prompt('What time is it? 0-23');
-var greeting;
-if(hourNow > 20 && hourNow < 24){
-    greeting = 'Go to Bed';
-} else if(hourNow >12 && hourNow < 20) {
-    greeting = 'Good day person.';
-} else if( hourNow >= 0 && hourNow < 24){
-    greeting = 'Go to work, and Good Morning.';
-} else {
-    greeting = 'Wait, you didnt give me the right response. Who knows what time it is?'
-}
-console.log(greeting);
-document.write(greeting);
-
 var d = new Date();
 var b = d.getFullYear();
 document.getElementById('copyright').innerHTML = b;
+
+var gaming = prompt('What kind of board games do you enjoy?');
+var greeting;
+if(gaming === 'euro'){
+    greeting = 'Ciao! Bienvenido! Bienvenue!';
+} else if(gaming === 'party'){
+    greeting = 'You can find plenty of friends here!';
+} else if(gaming === 'puzzle'){
+    greeting = 'uYo naC Fdni thaW uYo rAe ookLnig roF erHe.';
+} else if(gaming === 'secret identity'){
+    greeting = 'I see you are familiar with incognito.';
+} else if(gaming === 'combat'){
+    greeting = 'Grab the nearest sharp object and get in here!';
+} else if(gaming === 'legacy'){
+    greeting = 'Est. 1992';
+} else if(gaming === 'deck builder'){
+    greeting = 'Combining multiple things into one is what we do best here.';
+} else {
+    greeting = 'You probably said something awful like cooperative or uno. That is okay, we still welcome you.'
+}
+console.log(gaming);
+document.write(greeting);
+
+
+document.getElementById('clickhere').addEventListener("click", tellMe)
+function tellMe(){
+    var message = alert('You were not supposed to do that.');
+}
